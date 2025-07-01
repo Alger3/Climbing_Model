bd121e414f2995f9d0a1bc244601a1756cb058d4
jack <guanjieliang3@gmail.com>
2025-07-01 20:22:01 +0100

    Create function to predict feasibility

--- src/route_parser.py
+++ src/route_parser.py
@@ -1,5 +1,6 @@
 import json
 import networkx as nx
+from math import dist
 
 #Define a function to calculate the center of catch points
 def hold_centers(data):
@@ -32,3 +33,27 @@ def build_route_graph(route,threshold):
                 if dist <= threshold:
                     G.add_edge(i, j)
     return G
+
+
+# TODO: Need to complicated the algorithmn
+# assign the label to each athletes
+def is_route_feasible(climber,points):
+    height = climber["height"]
+    ape_index = climber["ape_index"]
+    weight = climber["weight"]
+    flexibility = climber["flexibility"]
+    strength = climber["strength"]
+
+    # Add condition to judge complete or not
+    reach = height * ape_index
+    flex_bonus = (flexibility/10)*0.15
+    strength_bonus = (strength/100)*0.2
+    weight_punish = weight*0.05
+
+    max_reach = reach * (1 + strength_bonus + flex_bonus) - weight_punish
+
+    for i in range(len(points)-1):
+        if dist(points[i],points[i+1]) > max_reach:
+            return False
+
+    return True
