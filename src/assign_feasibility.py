9a0fc774d6f390cf010387fdeff97a0327454cf0
jack <guanjieliang3@gmail.com>
2025-07-04 16:28:02 +0100

    add simplest feasibility, and group them together

---
+++ src/assign_feasibility.py
@@ -0,0 +1,35 @@
+from math import dist
+
+# TODO: Need to complicated the algorithmn
+def is_route_feasible(climber,points):
+    height = climber["height"]
+    ape_index = climber["ape_index"]
+    weight = climber["weight"]
+    flexibility = climber["flexibility"]
+    strength = climber["strength"]
+
+    # Add condition to judge complete or not
+    reach = height * ape_index
+    flex_bonus = (flexibility/10)*0.15
+    strength_bonus = (strength/100)*0.2
+    weight_punish = weight*0.05
+
+    max_reach = reach * (1 + strength_bonus + flex_bonus) - weight_punish
+
+    # TODO: I dont know how to define the metric to label whether they can pass
+    for i in range(len(points)-1):
+        if dist(points[i],points[i+1]) > max_reach:
+            return False
+
+    return True
+
+# TODO: the Climbing wall points are not in order, need to sort them first
+# TODO: Or need to only consider the closer point or try to follow the reachibility by Sophie
+def simplest_feasible_arm_span(climber,points):
+    height = climber["height"]
+    ape_index = climber["ape_index"]
+
+    arm_span = height * ape_index
+    max_dis = max(dist(points[i],points[i+1]) for i in range(len(points)-1))
+
+    return max_dis <= arm_span
