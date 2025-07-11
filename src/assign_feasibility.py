bc12f8ba00308fa569d13d7f8893896aafa01984
jack <guanjieliang3@gmail.com>
2025-07-11 17:00:37 +0100

    Change the dist to pixel_dist

--- src/assign_feasibility.py
+++ src/assign_feasibility.py
@@ -1,4 +1,5 @@
 from math import dist
+from route_parser import pixel_dist
 
 # TODO: Need to complicated the algorithmn
 def is_route_feasible(climber,points):
@@ -12,13 +13,13 @@ def is_route_feasible(climber,points):
     reach = height * ape_index
     flex_bonus = (flexibility/10)*0.15
     strength_bonus = (strength/100)*0.2
-    weight_punish = weight*0.05
+    weight_penalty = weight*0.05
 
-    max_reach = reach * (1 + strength_bonus + flex_bonus) - weight_punish
+    max_reach = reach * (1 + strength_bonus + flex_bonus) - weight_penalty
 
     # TODO: I dont know how to define the metric to label whether they can pass
     for i in range(len(points)-1):
-        if dist(points[i],points[i+1]) > max_reach:
+        if pixel_dist(points[i],points[i+1]) > max_reach:
             return False
 
     return True
@@ -26,11 +27,3 @@ def is_route_feasible(climber,points):
 # TODO: the Climbing wall points are not in order, need to sort them first
 # TODO: Or need to only consider the closer point or try to follow the reachibility by Sophie
 # TODO: 1. Use random one to pixel and cm 2. Use GNN 3. Use 分类模型
-def simplest_feasible_arm_span(climber,points):
-    height = climber["height"]
-    ape_index = climber["ape_index"]
-
-    arm_span = height * ape_index
-    max_dis = max(dist(points[i],points[i+1]) for i in range(len(points)-1))
-
-    return max_dis <= arm_span
