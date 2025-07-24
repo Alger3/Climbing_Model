dfe91a1aed3606f6a5611947f90419543736dced
jack <guanjieliang3@gmail.com>
2025-07-24 19:44:19 +0100

    Add ReachabilityGNN class

--- src/reachability_model_function.py
+++ src/reachability_model_function.py
@@ -1,5 +1,8 @@
 from torch_geometric.data import Data
 from route_parser import pixel_dist_to_cm
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
 import numpy as np
 import torch
 
@@ -28,6 +31,20 @@ def build_graph_reachability(route, hand_points, foot_points, climber, labels):
 
     num_nodes = len(route)
     edge_index = torch.combinations(torch.arange(num_nodes), r=2).T
+    # Let the edge become undirected edge
     edge_index = torch.cat([edge_index, edge_index[[1, 0]]], dim=1)
 
     return Data(x=x, edge_index=edge_index, y=y)
+
+class ReachabilityGNN(nn.Module):
+    def __init__(self, node_in=10, hidden=32, out=4):
+        super().__init__()
+        self.conv1 = GCNConv(node_in, hidden)
+        self.conv2 = GCNConv(hidden, hidden)
+        self.classifier = nn.Linear(hidden, out)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index))
+        return self.classifier(x)
