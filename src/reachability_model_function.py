bf868cd83f0b9ac6315bd0cb33ea4c872db83e34
jack <guanjieliang3@gmail.com>
2025-07-26 00:13:03 +0100

    Reachability Model Version 3

--- src/reachability_model_function.py
+++ src/reachability_model_function.py
@@ -37,43 +37,37 @@ def build_graph_reachability(route, hand_points, foot_points, climber, labels):
     # edge_index = torch.cat([edge_index, edge_index[[1, 0]]], dim=1)
 
     # KNN
-    num_nodes = len(route)
     route_array = np.array(route)
 
-    nbrs = NearestNeighbors(n_neighbors=6, algorithm='auto').fit(route_array)
+    nbrs = NearestNeighbors(n_neighbors=10, algorithm='auto').fit(route_array)
     _, indices = nbrs.kneighbors(route_array)
 
-    edges = []
+    edges = set()
     for i, neighbors in enumerate(indices):
         for j in neighbors:
             if i != j:
-                edges.append([i,j])
+                edges.add(tuple(sorted((i, j))))
     
-    edge_index = torch.tensor(edges, dtype=torch.long).T
-    edge_index = torch.cat([edge_index, edge_index[[1,0]]], dim=1)
+    edge_index = torch.tensor(list(edges), dtype=torch.long).T
 
     return Data(x=x, edge_index=edge_index, y=y)
 
 class ReachabilityGNN(nn.Module):
-    def __init__(self, node_in=10, hidden=32, out=4):
+    def __init__(self, node_in=10, hidden=64, out=4):
         super().__init__()
         self.conv1 = GCNConv(node_in, hidden)
         self.conv2 = GCNConv(hidden, hidden)
         self.conv3 = GCNConv(hidden, hidden)
-        self.classifier = nn.Linear(hidden, out)
-        self.dropout = nn.Dropout(0.3)
-        self.norm = nn.LayerNorm(hidden)
+        self.classifier = nn.Sequential(
+            nn.Linear(hidden, hidden // 2),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(hidden // 2, out)
+        )
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
         x = F.relu(self.conv1(x, edge_index))
-        x = self.norm(x)
-        x = self.dropout(x)
-
         x = F.relu(self.conv2(x, edge_index))
-        x = self.norm(x)
-        x = self.dropout(x)
-
         x = F.relu(self.conv3(x, edge_index))
-        x = self.norm(x)
         return self.classifier(x)
